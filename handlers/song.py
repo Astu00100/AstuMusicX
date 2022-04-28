999a75bc27db8c1ff33d2a683d4f7d15b0a66c80
Astu00800 <98279259+Astu00800@users.noreply.github.com>
2022-04-28 12:10:19 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ [❛-💔♣️ 卂𝓢𝓣𝓾 _𝑜𝓅 🔥💙](https://t.me/w2hsupport) ❤️**"
+        rep = "**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ [❛-💔♣️ 卂𝓢𝓣𝓾 _𝑜𝓅 🔥💙](https://t.me/Feelings_of_Astu) ❤️**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
